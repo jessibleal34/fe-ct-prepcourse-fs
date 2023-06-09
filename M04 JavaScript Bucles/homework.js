3ca0f2949376bb46470c29778668be8b74768baa
jessi34 <jessibleal34@gmail.com>
2023-06-09 02:35:37 -0500

    solucion homework 04

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -141,6 +141,15 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 3 === 0 && num % 5 === 0){
+      return "fizzbuzz"
+   } else if(num % 3 === 0){
+      return "fizz"
+    }  else if (num % 5 === 0){
+         return "buzz"
+   } else {
+      return false;
+   }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -151,6 +160,17 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if (num1 > num2 && num1 > num3 && num1 >= 0){
+      return "Numero 1 es mayor y positivo"
+   } else if (num1 < 0 || num2 < 0 || num3 < 0){
+      return "Hay negativos"
+   } else if(num3 > num1 && num3 > num2 ){
+      return (num3 + 1)
+   }else if( num1 === 0 && num2 === 0 && num3 === 0){
+      return "Error"
+   }else {
+      return false ;
+   }
 }
 
 function esPrimo(num) {
@@ -160,18 +180,41 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if ( num < 2){
+      return false;
+   }
+
+for (let i = 2; i < num; i++) 
+   if ( num % 1 === 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if (valor === true){
+      return "Soy verdadero"
+   }else{
+      return "Soy falso"
+   }
 }
 
 function tieneTresDigitos(num) {
+
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if (num > 99 && num < 1000){
+      return true;
+   }
+      else {
+         return false;
+      }
+   
 }
 
 function doWhile(num) {
@@ -179,6 +222,10 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   while(suma < 9){
+      suma = suma + 5;
+      return (suma);
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
